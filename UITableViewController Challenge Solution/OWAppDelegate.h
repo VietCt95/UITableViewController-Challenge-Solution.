7a63131876ab4f487d49f7fe031d646320364268
VietCT <vietct95@gmail.com>
2014-10-11 11:01:42 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by VietCT on 10/11/14.
+//  Copyright (c) 2014 FlyingPig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by VietCT on 10/11/14.
+//  Copyright (c) 2014 FlyingPig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
